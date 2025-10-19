8283abf92da04865ea456a259b0219570a400635
true-lnz <lansonzgroup@yandex.ru>
2025-10-19 22:38:16 +0500

    2 урок

---
+++ src/components/ToDoList/ToDoList.tsx
@@ -0,0 +1,16 @@
+import "./ToDoList.scss";
+import { ToDoListItem } from "./ToDoListItem/ToDoListItem";
+
+export const ToDoList = () => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list failed">
+        <ToDoListItem />
+      </ul>
+
+      <ul className="todo-list completed">
+        <ToDoListItem />
+      </ul>
+    </div>
+  );
+};
